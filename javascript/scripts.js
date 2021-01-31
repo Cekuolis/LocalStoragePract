1e4865ac2affca5c3eacceb707c2491acfdf8038
Cekuolis <valavicius.algimantas@gmail.com>
2021-01-31 18:06:33 +0200

    Added li

---
+++ javascript/scripts.js
@@ -0,0 +1,48 @@
+let forma = document.forms[0];
+let minusValue;
+let value;
+let array = [];
+let suma;
+var element = document.getElementById("sestas");
+const history = document.getElementById("history")
+let optionValue = "Add money";
+
+
+// isaugome skaicius i masyva jei add tai su + jei remove tai su -
+value = (e) => {
+  e.preventDefault();
+  value = e.target.numberInput.value;
+  if (document.getElementById("inputState").value === "Add money") {
+    array.push(value);
+    localStorage.setItem("money", JSON.stringify(array));
+  } else {
+    array.push(-value);
+    localStorage.setItem("money", JSON.stringify(array));
+  }
+  let petr = JSON.parse(localStorage.getItem("money"));
+
+  let totalMoney = getArraySum(petr);
+  element.innerHTML = totalMoney;
+
+  function getArraySum(a) {
+    var total = 0;
+    for (var i in a) {
+      total += parseInt(a[i]);
+    }
+    return total;
+  }
+  let li = document.createElement('li');
+  li.textContent = value;
+  history.appendChild(li);
+  
+
+ 
+
+};
+
+forma.addEventListener("submit", value);
+
+//////////////////////////////////////////////////////////////////////////
+
+/// spausdinimas i console local storage masyvas
+console.log(JSON.parse(localStorage.getItem("money")));
